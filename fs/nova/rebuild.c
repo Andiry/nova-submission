2bb379cd2a805659899c73439be758cd6afdc65d
Andiry Xu <jix024@cs.ucsd.edu>
2018-03-22 23:44:01 -0700

    Remove "radix" from dir insert/remove methods

--- fs/nova/rebuild.c
+++ fs/nova/rebuild.c
@@ -313,7 +313,7 @@ static inline int nova_replay_add_dentry(struct super_block *sb,
 		return -EINVAL;
 
 	nova_dbg_verbose("%s: add %s\n", __func__, entry->name);
-	return nova_insert_dir_radix_tree(sb, sih,
+	return nova_insert_dir_tree(sb, sih,
 			entry->name, entry->name_len, entry);
 }
 
@@ -322,7 +322,7 @@ static inline int nova_replay_remove_dentry(struct super_block *sb,
 	struct nova_inode_info_header *sih, struct nova_dentry *entry)
 {
 	nova_dbg_verbose("%s: remove %s\n", __func__, entry->name);
-	nova_remove_dir_radix_tree(sb, sih, entry->name,
+	nova_remove_dir_tree(sb, sih, entry->name,
 					entry->name_len, 1, NULL);
 	return 0;
 }
