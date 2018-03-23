616f6b7d923e62d8384597a3213be3d02177f606
Andiry Xu <jix024@cs.ucsd.edu>
2018-03-23 00:52:15 -0700

    Remove unnecessary sbi parameters

--- fs/nova/bbuild.c
+++ fs/nova/bbuild.c
@@ -53,7 +53,8 @@ void nova_init_header(struct super_block *sb,
 	init_rwsem(&sih->i_sem);
 }
 
-static inline int get_cpuid(struct nova_sb_info *sbi, unsigned long blocknr)
+static inline int get_block_cpuid(struct nova_sb_info *sbi,
+	unsigned long blocknr)
 {
 	return blocknr / sbi->per_list_blocks;
 }
@@ -134,12 +135,12 @@ static int nova_init_blockmap_from_inode(struct super_block *sb)
 			NOVA_ASSERT(0);
 		blknode->range_low = le64_to_cpu(entry->range_low);
 		blknode->range_high = le64_to_cpu(entry->range_high);
-		cpuid = get_cpuid(sbi, blknode->range_low);
+		cpuid = get_block_cpuid(sbi, blknode->range_low);
 
 		/* FIXME: Assume NR_CPUS not change */
 		free_list = nova_get_free_list(sb, cpuid);
-		ret = nova_insert_blocktree(sbi,
-				&free_list->block_free_tree, blknode);
+		ret = nova_insert_blocktree(&free_list->block_free_tree,
+				blknode);
 		if (ret) {
 			nova_err(sb, "%s failed\n", __func__);
 			nova_free_blocknode(sb, blknode);
@@ -446,7 +447,6 @@ inline void set_bm(unsigned long bit, struct scan_bitmap *bm,
 static int nova_insert_blocknode_map(struct super_block *sb,
 	int cpuid, unsigned long low, unsigned long high)
 {
-	struct nova_sb_info *sbi = NOVA_SB(sb);
 	struct free_list *free_list;
 	struct rb_root *tree;
 	struct nova_range_node *blknode = NULL;
@@ -464,7 +464,7 @@ static int nova_insert_blocknode_map(struct super_block *sb,
 		return -ENOMEM;
 	blknode->range_low = low;
 	blknode->range_high = high;
-	ret = nova_insert_blocktree(sbi, tree, blknode);
+	ret = nova_insert_blocktree(tree, blknode);
 	if (ret) {
 		nova_err(sb, "%s failed\n", __func__);
 		nova_free_blocknode(sb, blknode);
@@ -1093,7 +1093,7 @@ static int nova_failure_insert_inodetree(struct super_block *sb,
 	tree = &inode_map->inode_inuse_tree;
 	mutex_lock(&inode_map->inode_table_mutex);
 
-	ret = nova_find_free_slot(sbi, tree, internal_low, internal_high,
+	ret = nova_find_free_slot(tree, internal_low, internal_high,
 					&prev, &next);
 	if (ret) {
 		nova_dbg("%s: ino %lu - %lu already exists!: %d\n",
