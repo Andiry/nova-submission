e2a92c1049f8331d4c9d4ab0f669ec68d9ef4b15
Andiry Xu <andiryxu@google.com>
2018-08-15 02:17:48 -0700

    bdev_dax_supported fix for 4.18

--- fs/nova/super.c
+++ fs/nova/super.c
@@ -101,7 +101,7 @@ static int nova_get_nvmm_info(struct super_block *sb,
 	struct dax_device *dax_dev;
 	int ret;
 
-	ret = bdev_dax_supported(sb, PAGE_SIZE);
+	ret = bdev_dax_supported(sb->s_bdev, PAGE_SIZE);
 	nova_dbg_verbose("%s: dax_supported = %d; bdev->super=0x%p",
 			 __func__, ret, sb->s_bdev->bd_super);
 	if (ret) {
